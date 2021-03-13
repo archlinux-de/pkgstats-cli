debcce76feec7b6eb42194216ca7805f6d3b5948
Pierre Schmitz <pierre@archlinux.de>
2021-03-13 15:59:08 +0100

    Use uname syscall instead of calling the program

--- internal/system/system.go
+++ internal/system/system.go
@@ -1,29 +1,9 @@
 package system
 
-import (
-	"os/exec"
-	"strings"
-)
-
 type System struct {
-	env     []string
-	uname   string
 }
 
 func NewSystem() System {
 	system := System{}
-	system.uname = "uname"
 	return system
-}
-
-func (system *System) GetArchitecture() (string, error) {
-	arch, err := system.getMachine()
-	return arch, err
-}
-
-func (system *System) getMachine() (string, error) {
-	cmd := exec.Command(system.uname, "-m")
-	cmd.Env = system.env
-	out, err := cmd.Output()
-	return strings.TrimSpace(string(out)), err
 }
