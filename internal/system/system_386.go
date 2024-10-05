3891a1d54fae1ef79c6301a8593a110b2d0ea127
Pierre Schmitz <pierre@archlinux.de>
2024-10-05 14:22:36 +0200

    Detect i586 and i686

--- internal/system/system_386.go
+++ internal/system/system_386.go
@@ -5,8 +5,10 @@ import (
 )
 
 func (system *System) GetCpuArchitecture() (architecture string, err error) {
+	// Check if we are on i686; otherwiese we know it is i586 with MMX as Go does require MMX instructions
+	isI686 := hasCMOV()
 	// We need to check for LM (Long Mode) as there are CPUs that support SSE2 but not x86_64 (e.g. Core Duo)
-	isX86_64 := system.hasLM() && cpu.X86.HasSSE2
+	isX86_64 := isI686 && hasLM() && cpu.X86.HasSSE2
 	isX86_64V2 := isX86_64 && cpu.X86.HasPOPCNT && cpu.X86.HasSSE3 && cpu.X86.HasSSE41 && cpu.X86.HasSSE42 && cpu.X86.HasSSSE3
 	isX86_64V3 := isX86_64V2 && cpu.X86.HasAVX && cpu.X86.HasAVX2 && cpu.X86.HasBMI1 && cpu.X86.HasBMI2 && cpu.X86.HasFMA && cpu.X86.HasOSXSAVE
 	isX86_64V4 := isX86_64V3 && cpu.X86.HasAVX512F && cpu.X86.HasAVX512BW && cpu.X86.HasAVX512CD && cpu.X86.HasAVX512DQ && cpu.X86.HasAVX512VL
@@ -20,17 +22,15 @@ func (system *System) GetCpuArchitecture() (architecture string, err error) {
 		architecture = "x86_64_v2"
 	case isX86_64:
 		architecture = "x86_64"
-	default:
+	case isI686:
 		architecture = "i686"
+	default:
+		architecture = "i586"
 	}
 
 	return
 }
 
 // Implemented at system_386.s
-func cpuid() uint32
-
-func (system *System) hasLM() bool {
-	// If Long Mode is available the 29th bit is set
-	return cpuid()&(1<<29) != 0
-}
+func hasLM() bool
+func hasCMOV() bool
--- internal/system/system_386.s
+++ internal/system/system_386.s
@@ -1,9 +1,19 @@
 #include "textflag.h"
 
-// func cpuid() uint32
-TEXT ·cpuid(SB), NOSPLIT, $0-4
-	MOVL $0x80000001, AX
-	MOVL $0, CX
-	CPUID
-	MOVL DX, ret+0(FP)
-	RET
+// func hasLM() bool
+TEXT ·hasLM(SB), NOSPLIT, $0-1
+    MOVL $0x80000001, AX  // Use extended function 0x80000001
+    CPUID
+    SHRL $29, DX          // Shift right by 29 bits
+    ANDL $1, DX           // Isolate the 29th bit
+    MOVB DX, ret+0(FP)    // Move the result to the return value
+    RET
+
+// func hasCMOV() bool
+TEXT ·hasCMOV(SB), NOSPLIT, $0-1
+    MOVL $1, AX  // Use standard function 1
+    CPUID
+    SHRL $15, DX // Shift right by 15 bits
+    ANDL $1, DX  // Isolate the 15th bit
+    MOVB DX, ret+0(FP)  // Move the result to the return value
+    RET
