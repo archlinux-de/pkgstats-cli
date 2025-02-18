579cf3929f3da9cdf50506a962734cccff6a7072
Pierre Schmitz <pierre@archlinux.de>
2025-02-18 19:20:19 +0100

    Try to improve armv7 detection

--- internal/system/system_arm.go
+++ internal/system/system_arm.go
@@ -9,10 +9,12 @@ func (s *System) GetCpuArchitecture() (string, error) {
 	// https://github.com/golang/go/issues/38987#issuecomment-626513091
 	// https://community.arm.com/developer/tools-software/oss-platforms/b/android-blog/posts/runtime-detection-of-cpu-features-on-an-armv8-a-cpu
 	// https://developer.arm.com/documentation/dui0471/m/key-features-of-arm-architecture-versions/arm-architecture-v7-a?lang=en
+	// https://gcc.gnu.org/wiki/ARMv7
+	// https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
 	switch {
 	case ARM.HasPMULL || ARM.HasCRC32 || ARM.HasAES || ARM.HasSHA1 || ARM.HasSHA2:
 		return AARCH64, nil
-	case ARM.HasVFPv3 && ARM.HasTHUMBEE:
+	case (ARM.HasVFPv3 && ARM.HasTHUMBEE) || (ARM.HasTHUMB && ARM.HasNEON):
 		return ARMV7, nil
 	case ARM.HasVFP && ARM.HasTHUMB:
 		return ARMV6, nil
